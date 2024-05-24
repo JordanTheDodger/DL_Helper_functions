ab3ca0bf0e1eb4cf4afa57b7d9c94e64495ad149
JordanTheDodger <54824287+JordanTheDodger@users.noreply.github.com>
2024-05-24 11:36:57 -0400

    file update

--- funlearning_DNA_biopython.py
+++ funlearning_DNA_biopython.py
@@ -124,10 +124,10 @@ class dna_tool_sets ():
         for header, dna_seq in self.dict.items(): # generate orf for the whole file
             pos = self.find_pos(dna_seq)
             orf[header] = pos
-        # find header for question 7
+        #find header
         id_key = [key for key in orf if "gi|142022655|gb|EQ086233.1|129" in key]
         idx = id_key[0]  
-        # generate list of frames for questions 4 to 7
+        
         frame1, frame2, all_frames, id_frames = [], [], [], []
         for key, dict_value in orf.items():
             frame1 += dict_value["frame1"]
@@ -137,27 +137,22 @@ class dna_tool_sets ():
             if key == idx:
                 id_frames = dict_value["frame1"] + dict_value["frame2"] + dict_value["frame3"]
             
-        #Q4: What is the length of the longest ORF appearing in reading
-        #frame 2 of any of the sequences?
+        
         frame2_max_length = max(frame2, key = lambda x: x[1])
-        print("Q4: The length of longest ORF in frame2: %d\n"%frame2_max_length[1])
-        #Q5: What is the starting position of the longest ORF in reading frame 1 
-        #in any of the sequences? 
+        print("The length of longest ORF in frame2: %d\n"%frame2_max_length[1])
+        
         
         frame1_max_length_pos = max(frame1, key = lambda x: x[1])
-        print ("Q5: The start position of longest ORF in frame1: %d\n"%frame1_max_length_pos[0])
-        #Q6: What is the length of the longest ORF appearing in any sequence and 
-        #in any forward reading frame?
+        print ("The start position of longest ORF in frame1: %d\n"%frame1_max_length_pos[0])
+        
         max_length = max(all_frames, key = lambda x: x[1])
-        print ("Q6: The longest ORF of all frames and sequences: %d\n"%max_length[1])
-        #Q7: What is the length of the longest forward ORF that appears in the 
-        #sequence with the identifier gi|142022655|gb|EQ086233.1|129?
+        print ("The longest ORF of all frames and sequences: %d\n"%max_length[1])
+        
 
         max_length_id = max(id_frames, key = lambda x: x[1])
-        print ("Q7: The length of longest ORF for ", idx, "is: %d \n" %max_length_id[1])
+        print ("The length of longest ORF for ", idx, "is: %d \n" %max_length_id[1])
         
-        # uncomment the return code to return the whole orf information 
-        #including starting position and length of each frame for each sequence
+        # uncomment to return the whole orf information 
         
         #return orf
             
@@ -199,60 +194,35 @@ class dna_tool_sets ():
                 else:
                     combined_repeats[key] = combined_repeats.get(key) \
                                             + dict_value[key]
-        # Q8:Find the most frequently occurring repeat of length 7 in all 
-        # sequences. How many times does it occur in all?
+        
         if n == 7:
             most_freq_7 = max (combined_repeats.values())
-            print ("Q8: The most frequently repeats occur: %d times \n"%most_freq_7)
-        # Q10:Which one of the following repeats of length 7 has a maximum 
-        #number of occurrences?
+            print ("The most frequently repeats occur: %d times \n"%most_freq_7)
+        
             most_freq_7_seq = [key for key in combined_repeats if \
                        combined_repeats[key] == max(combined_repeats.values())]
-            print ("Q10: The following repeats occured most frequently: \n",most_freq_7_seq)
-        # Q9:Find all repeats of length 10 in the input file. Let's use Max to 
-        #specify the number of copies of the most frequent repeat of length 10. 
-        #How many different 10-base sequences occur Max times?
+            print ("The following repeats occured most frequently: \n",most_freq_7_seq)
+        
         if n == 10:
-            # answer question 9
+        
             count_most_freq_10 = len([value for value in combined_repeats.values()\
                              if value == max(combined_repeats.values())])
         
         
-            print ("Q9: The number of different 10-base sequences occur max times: %d \n"\
+            print ("The number of different 10-base sequences occur max times: %d \n"\
                   %count_most_freq_10)
         
-        # uncomment the return code to return the repeats with counts for each record 
-        # (repeats_set) and for the whole file (combined_repeats)
+        # uncomment to return the repeats with counts for each record 
+        # (repeats_set) and whole file (combined_repeats)
         #return repeats_set, combined_repeats
         
 if __name__ == "__main__":
     
     file_name = "../data/dna3.fasta"
     dna_tools = dna_tool_sets (file_name)
-    # Q1: How many records are in the multi-FASTA file?
+    
     dna_tools.count_records()
-    
-    # Q2: What is the length of the longest sequence in the file?
-    # Q3: What is the length of the shortest sequence in the file?
     dna_tools.check_length()
-    
-    #Q4: What is the length of the longest ORF appearing in reading
-    #frame 2 of any of the sequences?
-    #Q5: What is the starting position of the longest ORF in reading frame 1 
-    #in any of the sequences? 
-    #Q6: What is the length of the longest ORF appearing in any sequence and 
-    #in any forward reading frame?
-    #Q7: What is the length of the longest forward ORF that appears in the 
-    #sequence with the identifier gi|142022655|gb|EQ086233.1|129?
     dna_tools.orf_identifier()
-    
-    # Q8:Find the most frequently occurring repeat of length 7 in all 
-    # sequences. How many times does it occur in all?
-    # Q10:Which one of the following repeats of length 7 has a maximum 
-    #number of occurrences?
     dna_tools.repeats_identifier(7)
-    
-    # Q9:Find all repeats of length 10 in the input file. Let's use Max to 
-    #specify the number of copies of the most frequent repeat of length 10. 
-    #How many different 10-base sequences occur Max times?
     dna_tools.repeats_identifier(10)
